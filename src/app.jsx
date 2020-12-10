fa513483e5ba6d73e6aed02a9c14ccdf2ed87572
Jordan Elley <jordanelley@hotmail.com>
2020-12-11 00:28:01 +1300

    added basic home page

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import './app.css';
 
 const App = () => (
-  <div>hello world</div>
+    <div>
+        <h1>Ski fields</h1>
+        <table>
+            <tr>
+                <th> ski field </th>
+                <th> rating </th>
+            </tr>
+            <tr>
+                <td> Cadrona </td>
+                <td> 5 stars </td>
+            </tr>
+            <tr>
+                <td> Whakapapa </td>
+                <td> 2 stars </td>
+            </tr>
+        </table>
+    </div>
 )
 
 export default App;
