8e2c99b4348a28d52a63d11069c80a9b5ed2f7ff
Jordan Elley <jordanelley@hotmail.com>
2020-12-29 21:35:35 +1300

    added a test

---
+++ src/__tests__/app.tests.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, fireEvent, waitFor, waitForElementToBeRemoved, screen } from '@testing-library/react';
+import nock from 'nock';
+import App from '../app';
+import '@testing-library/jest-dom/extend-expect';
+
+//todo when the app starts, renders existing ratings
+
+describe('Ski field rating application', () => {
+    it('Should allow user to add a ski field rating', async () => {
+        nock('http://localhost:5000')
+            .defaultReplyHeaders({
+                'access-control-allow-origin': '*',
+                'access-control-allow-credentials': 'true'
+            })
+            .get('/reviews')
+            .reply(200, []);
+        nock('http://localhost:5000')
+            .defaultReplyHeaders({
+                'access-control-allow-origin': '*',
+                'access-control-allow-credentials': 'true'
+            })
+            .post('/reviews')
+            .reply(200)
+            .get('/reviews')
+            .reply(200, ['Test ski field 5 stars']);
+
+        render(<App />);
+
+// We must wait for this to avoid updated state after the component is unmounted.
+        await waitFor(() => screen.getByText('name :'));
+        const skiFieldInput = screen.getByLabelText('name :');
+        const skiRatingInput = screen.getByLabelText('rating :');
+
+        fireEvent.change(skiFieldInput, { target: { value: 'Test ski field' } })
+        fireEvent.change(skiRatingInput, { target: { value: '5 stars' } })
+
+        const submitButton = screen.getByText('submit');
+        fireEvent.click(submitButton);
+        const testSkiFieldRating = await waitFor(() => screen.getByText('Test ski field 5 stars'));
+        expect(testSkiFieldRating).toBeInTheDocument();
+    })
+    }
+
+)
