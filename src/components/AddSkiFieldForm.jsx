94cdbd4448747f307701829102adba940a976b81
Jordan Elley <jordanelley@hotmail.com>
2020-12-29 21:39:54 +1300

    added a link from the labels to the fields

--- src/components/AddSkiFieldForm.jsx
+++ src/components/AddSkiFieldForm.jsx
@@ -10,10 +10,10 @@ function AddSkiFieldForm({onSubmit}){
          <div>
             <h1>Know of a ski field which isn't listed? Add a new one</h1>
             <form>
-                <label id="name">name :</label>
-                <input type="text" onChange={(event) => setName(event.target.value)}/>
-                <label id="rating">rating :</label>
-                <input type="text" onChange={(event) => setReview(event.target.value)}/>
+                <label id="name" htmlFor='name_input'>name :</label>
+                <input type="text" id='name_input' onChange={(event) => setName(event.target.value)}/>
+                <label id="rating" htmlFor='rating_input'>rating :</label>
+                <input type="text" id='rating_input' onChange={(event) => setReview(event.target.value)}/>
             <button type='button' onClick={async () => {
                  await addSkiFieldReview(name,review)
                 onSubmit();
