535968c92867baa3d012cd26cbfa6277ce29c08e
Jordan Elley <jordanelley@hotmail.com>
2021-04-10 17:05:38 +1200

    updated config

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
 set -e
-aws s3api put-object --bucket skifield-frontend --key index.html --body ../dist/index.html --cache-control "public" --content-type "text/html; charset=utf-8" >/dev/null
-aws s3api put-object --bucket skifield-frontend --key index.js --body ../dist/index.js --cache-control "public" --content-type "application/javascript" >/dev/null
-aws s3 cp ../dist/images/ s3://skifield-frontend/images/ --recursive
+aws s3api put-object --bucket skifield-frontend --key index.html --body ./dist/index.html --cache-control "public" --content-type "text/html; charset=utf-8" >/dev/null
+aws s3api put-object --bucket skifield-frontend --key index.js --body ./dist/index.js --cache-control "public" --content-type "application/javascript" >/dev/null
+aws s3 cp ./dist/images/ s3://skifield-frontend/images/ --recursive
